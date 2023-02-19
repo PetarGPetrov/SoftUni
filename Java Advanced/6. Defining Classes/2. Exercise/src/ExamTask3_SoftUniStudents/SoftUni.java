cacdd22a1bf8cefd62a2266f239bc2bb45bf6242
PetarGPetrov <106280404+PetarGPetrov@users.noreply.github.com>
2023-02-19 22:32:27 +0000

    Add

--- Java Advanced/6. Defining Classes/2. Exercise/src/ExamTask3_SoftUniStudents/SoftUni.java
+++ Java Advanced/6. Defining Classes/2. Exercise/src/ExamTask3_SoftUniStudents/SoftUni.java
@@ -2,47 +2,40 @@ package ExamTask3_SoftUniStudents;
 
 import java.util.ArrayList;
 import java.util.List;
-// next write java class SoftUni that has students (a collection which stores entity students). All entities inside the repository
-// have the same fields. Also the soft uni class should have those 2 fields:
-// capacity: int
-//: data: List<Students>
+
 
 public class SoftUni {
-    private int capacity;// capacity: int
+    private int capacity;
 
-    List<Student> data;//: data: List<Students> и това поле ми е равно на null в момента
-    //полета които са null на практика не съществуват
-    //условието тук ни кара да направим конструктор само на capacity
-    //ние слагаме под него data която да е равна на нов празен лист. празният лист е празен но не е null което ни позволява да го ползваме
+    List<Student> data;
+   
     public SoftUni(int capacity) {
         this.capacity = capacity;
         this.data = new ArrayList<>();
     }
 
-    public int getCapacity() {   // условие getCapacity()
+    public int getCapacity() {   
         return capacity;
     }
 
-    public int getCount(){  // условие getCount() method returns the number of students, and we get them from the list
+    public int getCount(){ 
         return this.data.size();
     }
 
-    public String insert(Student student){ // условие insert (Student student) method adds and entity to the data if hall for it
-        // return Added Student {FirstName Last name }if student succesfully added
+    public String insert(Student student){ 
 
 
 
 
 
-
-        // return The hall is full if the hall is full
-        if (getCount() + 1 > this.capacity){ //get count е броят на студенти(ако той +1 е по голям от капацитета)
+       
+        if (getCount() + 1 > this.capacity){  
             return "The hall is full.";
-        }else {  // return Student is already in the hall if student is already in the list
+        }else {  
             if (this.data.contains(student)){
                 return "Student is already in the hall.";
-            }else {// return Added Student {FirstName Last name }if student succesfully added
-                this.data.add(student); // добавяме си студента в списъка
+            }else {
+                this.data.add(student); 
                 return String.format("Added student %s %s", student.getFirstName(), student.getLastName());
 
         }
@@ -50,19 +43,19 @@ public class SoftUni {
         }
     }
 
-    public String remove(Student student){ // условие remove (Student student) method - removes student
-        // return Removed {firstName lastName} student if student is successfully removed
+    public String remove(Student student){ 
+        
             if(this.data.contains(student)){
                 data.remove(student);
                 return String.format("Student %s %s.", student.getFirstName(), student.getLastName());
 
-            }else{ // return stundent not found if the student not in the hall
+            }else{
                 return "Student not found.";
             }
     }
 
-    public Student getStudent(String firstName, String lastName){ // условие getStudent(String firstName, String lastName)method - returns the student with given names
-        for (Student studentEba4 : this.data){ // за всеки един студент от нашият лист
+    public Student getStudent(String firstName, String lastName){ 
+        for (Student studentEba4 : this.data){ 
             if (studentEba4.getFirstName().equals(firstName) && studentEba4.getLastName().equals(lastName)){
                 return studentEba4;
             }
@@ -70,10 +63,10 @@ public class SoftUni {
         return null;
     }
 
-    public String statistics(){// условие getStatistics() return a string in the following format
+    public String statistics(){
         StringBuilder sb = new StringBuilder();
-        sb.append("Hall size: ").append(getCount()).append(System.lineSeparator());//1ви ред append aka добави текст hall size, взимаме размера с get count и слагаме сепаратор винаги
-        this.data.forEach(student -> sb.append(student.toString()).append(System.lineSeparator())); // за всеки студент в списъка append-ваме данните на студента във формата toString който пренаписахме
-        return sb.toString(); // взимаме всичко дето сме апенднали в билдъра форматирано в toString
+        sb.append("Hall size: ").append(getCount()).append(System.lineSeparator()); 
+        this.data.forEach(student -> sb.append(student.toString()).append(System.lineSeparator()));
+        return sb.toString(); 
     }
 }
--- Java Advanced/6. Defining Classes/2. Exercise/src/ExamTask3_SoftUniStudents/Student.java
+++ Java Advanced/6. Defining Classes/2. Exercise/src/ExamTask3_SoftUniStudents/Student.java
@@ -8,10 +8,9 @@ public class Student {
 
     private String bestCourse;
 
-    //конструктор който приема тези 3 параметра
-    //демек като кажа в мейн метода стринг студент прима 1 2 3 от някой масив това тря да са 1во име 2ро име и най добър курс
+   
     public Student(String firstName, String lastName, String bestCourse) {
-        //това за момента е празен обект.
+        
         this.firstName = firstName;
         this.lastName = lastName;
         this.bestCourse = bestCourse;
@@ -44,7 +43,7 @@ public class Student {
     }
 
     @Override
-    public String toString() {    // искаме да връща "Student: {firstName} {lastName}, Best Course = {BestCourse}"
+    public String toString() {    
          return String.format("Student: %s %s, Best Course = %s", getFirstName(), getLastName(), getBestCourse());
     }
 }
